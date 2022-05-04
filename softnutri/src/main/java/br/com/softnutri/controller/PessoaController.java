36223166adfd8fbf85e1c761ebafeb3710b8e815
rafael <rafael.barreto@meta.com.br>
2022-05-04 14:22:05 -0300

    teste

--- softnutri/src/main/java/br/com/softnutri/controller/PessoaController.java
+++ softnutri/src/main/java/br/com/softnutri/controller/PessoaController.java
@@ -1,15 +1,22 @@
 package br.com.softnutri.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.softnutri.controller.form.PessoaForm;
 import br.com.softnutri.dominio.Pessoa;
 import br.com.softnutri.dto.PessoaDto;
 import br.com.softnutri.repository.PessoaRepository;
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -27,6 +34,16 @@ public class PessoaController {
 			List<Pessoa> pessoas = pessoaRepository.findByNome(nome);
 			return PessoaDto.converter(pessoas);
 		}
+	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<PessoaDto> cadastrar(@RequestBody PessoaForm form, UriComponentsBuilder uriBuilder) {
+		Pessoa pessoa = form.converter(pessoaRepository);
+		pessoaRepository.save(pessoa);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(pessoa.getIdPessoa()).toUri();
+		return ResponseEntity.created(uri).body(new PessoaDto(pessoa));
 	}
 
 }
---
+++ softnutri/src/main/java/br/com/softnutri/controller/form/PessoaForm.java
@@ -0,0 +1,13 @@
+package br.com.softnutri.controller.form;
+
+import br.com.softnutri.dominio.Pessoa;
+import br.com.softnutri.repository.PessoaRepository;
+
+public class PessoaForm {
+
+	public Pessoa converter(PessoaRepository pessoaRepository) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
