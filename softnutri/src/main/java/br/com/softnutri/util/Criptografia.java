de85b29241638e1a3b036c9c7502c5436901c41f
rafael <rafael.barreto@meta.com.br>
2022-05-05 11:32:54 -0300

    alteração

--- softnutri/src/main/java/br/com/softnutri/util/Criptografia.java
+++ softnutri/src/main/java/br/com/softnutri/util/Criptografia.java
@@ -1,7 +1,7 @@
 package br.com.softnutri.util;
 
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class Criptografia {
@@ -9,20 +9,21 @@ public class Criptografia {
 	protected Criptografia() {
 		super();
 	}
-	@Autowired
-	private static PasswordEncoder passEncoder;
 
 	public static String encode(String encodedString) {
 		return new String(Base64.encodeBase64(encodedString.getBytes()));
 	}
-	
 
 	public static String decode(String encodedString) {
 		return new String(Base64.decodeBase64(encodedString.getBytes()));
 	}
 
 	public static String encoderSecurity(String senha) {
-		return passEncoder.encode(senha);
+		return passwordEncoder().encode(senha);
+	}
+
+	private static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
