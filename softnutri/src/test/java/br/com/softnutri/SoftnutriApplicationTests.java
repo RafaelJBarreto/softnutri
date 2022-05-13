f10b52fea06dc2a13074b6317aa5c153898a600a
rafael <rafael.barreto@meta.com.br>
2022-05-13 12:17:29 -0300

    teste

--- softnutri/src/test/java/br/com/softnutri/SoftnutriApplicationTests.java
+++ softnutri/src/test/java/br/com/softnutri/SoftnutriApplicationTests.java
@@ -1,30 +1,20 @@
 package br.com.softnutri;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import br.com.softnutri.domain.Module;
-import br.com.softnutri.domain.ModuleRole;
 import br.com.softnutri.domain.Nutritionist;
-import br.com.softnutri.domain.Paper;
 import br.com.softnutri.domain.Person;
 import br.com.softnutri.domain.User;
 import br.com.softnutri.enuns.Gender;
 import br.com.softnutri.enuns.UserType;
-import br.com.softnutri.repository.ModuleRoleRepository;
-import br.com.softnutri.repository.ModuleRepository;
-import br.com.softnutri.repository.PaperRepository;
 import br.com.softnutri.repository.PersonRepository;
 import br.com.softnutri.repository.UserRepository;
-import br.com.softnutri.util.Criptografia;
 
 @SpringBootTest
 class SoftnutriApplicationTests {
@@ -33,20 +23,10 @@ class SoftnutriApplicationTests {
 
 	private final UserRepository usuarioRepository;
 
-	private final PaperRepository papelRepository;
-	
-	private final ModuleRepository moduloRepository;
-	
-	private final ModuleRoleRepository moduloPapelRepository;
-
 	@Autowired
-	public SoftnutriApplicationTests(PersonRepository pessoaRepository, UserRepository usuarioRepository,
-			PaperRepository papelRepository, ModuleRepository moduloRepository, ModuleRoleRepository moduloPapelRepository) {
+	public SoftnutriApplicationTests(PersonRepository pessoaRepository, UserRepository usuarioRepository) {
 		this.pessoaRepository = pessoaRepository;
 		this.usuarioRepository = usuarioRepository;
-		this.papelRepository = papelRepository;
-		this.moduloRepository = moduloRepository;
-		this.moduloPapelRepository = moduloPapelRepository;
 	}
 
 	@Test
@@ -104,62 +84,5 @@ class SoftnutriApplicationTests {
 
 		assertEquals(p.getEmail(), nc.getEmail());
 
-	}
-
-	@Test
-	void testaCriarPapel() {
-
-		Paper p = new Paper();
-		p.setDescription("Usuario");
-
-		Paper nc = papelRepository.save(p);
-
-		assertNotNull(nc.getIdPaper());
-
-	}
-
-	@Test
-	void testaCriarModulo() {
-
-		Module m = new Module();
-		m.setName("Cadastro");
-		m.setOrders(1);
-		m.setPathBase("app/cadastro");
-		m.setIcon(null);
-
-		Module nc = moduloRepository.save(m);
-
-		assertNotNull(nc.getIdModule());
-
-	}
-
-	@Test
-	void testaAssociacaoPapelModulo() {
-
-		ModuleRole mp = new ModuleRole();
-		mp.setModule(moduloRepository.findById(1L).get());
-		mp.setPaper(papelRepository.findById(1L).get());
-		
-		ModuleRole nc = moduloPapelRepository.save(mp);
-
-		assertNotNull(nc.getIdModuleRole());
-
-	}
-	
-	@Test
-	void testaAssociacaoUsuarioPapel() {
-
-		User u = usuarioRepository.findByEmail(Criptografia.encode("ana@outlook.com.br")).get();
-		 Set<Paper> papel = new HashSet<Paper>() ;
-		 papel.add(papelRepository.findById(1L).get());
-		u.setPapel(papel);
-		
-		User nc = usuarioRepository.save(u);
-
-		assertNotNull(nc.getPapel());
-
-	}
-	
-	
-
+	}	
 }
