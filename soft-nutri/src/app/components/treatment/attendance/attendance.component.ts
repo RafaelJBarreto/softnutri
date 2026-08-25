04bcb68da8aa16a4e574b73d63cbf0341cace2f6
Rafael Barreto <43484967+RafaelJBarreto@users.noreply.github.com>
2026-08-25 20:17:33 -0300

    Fix AttendanceComponent standalone imports

--- soft-nutri/src/app/components/treatment/attendance/attendance.component.ts
+++ soft-nutri/src/app/components/treatment/attendance/attendance.component.ts
@@ -1,4 +1,17 @@
-import * as _ from 'lodash';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatOption } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { Snack } from 'src/app/model/snack/snack';
@@ -9,36 +22,45 @@ import { FoodBunchService } from 'src/app/services/foodBunch/foodBunch.service';
 import { SnackService } from 'src/app/services/snack/snack.service';
 import { TableService } from 'src/app/services/table/table.service';
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatOption } from '@angular/material/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { TranslateService } from '@ngx-translate/core';
+import { FoodAttendanceComponent } from './food-attendance/food-attendance.component';
 
 @Component({
   selector: 'app-attendance',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    TranslatePipe,
+    FoodAttendanceComponent,
+  ],
   templateUrl: './attendance.component.html',
   styleUrls: ['./attendance.component.scss']
 })
-
-
 export class AttendanceComponent implements OnInit {
   errorMessage: any;
   tableControl = new FormControl();
-  filteredOptionsTable: Observable<Table[]>  = new Observable;
-  table: Table = new Table;
+  filteredOptionsTable: Observable<Table[]> = new Observable();
+  table: Table = new Table();
   tables: Table[] = [];
   idTable!: number;
   listBreakFast!: Array<FoodMenu>;
   step = 0;
-  loadingFoods: boolean = false;
+  loadingFoods = false;
   snacks: Snack[] = [];
   listSnackMenu!: Array<SnackMenu>;
   @Input() foodMenu!: FoodMenu;
-  caloriesSum: number = 0;
-  proteinSum: number = 0;
-  carbohydrateSum: number = 0;
-  lipidsSum: number = 0;
+  caloriesSum = 0;
+  proteinSum = 0;
+  carbohydrateSum = 0;
+  lipidsSum = 0;
 
   constructor(
     public snackService: SnackService,
@@ -46,25 +68,23 @@ export class AttendanceComponent implements OnInit {
     public tableService: TableService,
     public translate: TranslateService,
     private snackBar: MatSnackBar
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.listSnack();
     this.listTable();
-    this.listBreakFast = new Array
-    this.listSnackMenu = new Array;
+    this.listBreakFast = new Array();
+    this.listSnackMenu = new Array();
   }
 
   private listTable() {
     this.tableService.listAll().then(
       (data) => {
         this.tables = data;
-        this.filteredOptionsTable = this.tableControl.valueChanges
-          .pipe(
-            startWith(''),
-            map((name) => name ? this.filterTable(name) : this.tables.slice())
-          );
+        this.filteredOptionsTable = this.tableControl.valueChanges.pipe(
+          startWith(''),
+          map((name) => name ? this.filterTable(name) : this.tables.slice())
+        );
       })
       .catch((error) => {
         this.errorMessage = error.message;
@@ -72,11 +92,10 @@ export class AttendanceComponent implements OnInit {
           horizontalPosition: 'right',
           verticalPosition: 'top',
           duration: 3000
-
         });
-      })
-    };
-    
+      });
+  }
+
   private listSnack() {
     this.snackService.listAll().then(
       (data) => {
@@ -87,14 +106,13 @@ export class AttendanceComponent implements OnInit {
           horizontalPosition: 'right',
           verticalPosition: 'top',
           duration: 3000
-
         });
-    });
+      });
   }
 
   private filterTable(value: any) {
     let filterValue = '';
-    if (typeof value === "string") {
+    if (typeof value === 'string') {
       filterValue = value.toLowerCase();
     } else {
       filterValue = value.name.toLowerCase();
@@ -106,15 +124,17 @@ export class AttendanceComponent implements OnInit {
   }
 
   public selectedSnack(idSnack: number, idSnackMenu: number) {
-    for(let sm = 0; sm < this.listSnackMenu.length; sm++){
-      if(idSnackMenu === this.listSnackMenu[sm].idSnackMenu){
-        for(let s = 0; s < this.listSnackMenu[sm].listSnackMenu.length; s++){
-          if(idSnack === this.listSnackMenu[sm].listSnackMenu[s].idSnack){
-            this.listSnackMenu[sm].listSnackMenu[s].selected = this.listSnackMenu[sm].listSnackMenu[s].selected ? false : true;
-            if(this.listSnackMenu[sm].listSnackMenu[s].selected && this.listSnackMenu[sm].listBreakFast.length == 0){
-              this.listSnackMenu[sm].listBreakFast.push(new FoodMenu(this.listSnackMenu[sm].listBreakFast.length + 1, idSnackMenu, 0, 0, 0, 0, 0, 0));
+    for (let sm = 0; sm < this.listSnackMenu.length; sm++) {
+      if (idSnackMenu === this.listSnackMenu[sm].idSnackMenu) {
+        for (let s = 0; s < this.listSnackMenu[sm].listSnackMenu.length; s++) {
+          if (idSnack === this.listSnackMenu[sm].listSnackMenu[s].idSnack) {
+            this.listSnackMenu[sm].listSnackMenu[s].selected = !this.listSnackMenu[sm].listSnackMenu[s].selected;
+            if (this.listSnackMenu[sm].listSnackMenu[s].selected && this.listSnackMenu[sm].listBreakFast.length === 0) {
+              this.listSnackMenu[sm].listBreakFast.push(
+                new FoodMenu(this.listSnackMenu[sm].listBreakFast.length + 1, idSnackMenu, 0, 0, 0, 0, 0, 0)
+              );
             }
-          }else{
+          } else {
             this.listSnackMenu[sm].listSnackMenu[s].selected = false;
           }
         }
@@ -129,39 +149,39 @@ export class AttendanceComponent implements OnInit {
   }
 
   autoCompleteDisplayTable(item: any): string {
-    if (item == undefined || item == "") { 
+    if (item == undefined || item === '') {
       return '';
     }
     return item.name;
   }
 
-
   setFoodBreakFast(idSnackMenu: number) {
-    for(let sm = 0; sm < this.listSnackMenu.length; sm++){
-      if(idSnackMenu === this.listSnackMenu[sm].idSnackMenu){
-        this.listSnackMenu[sm].listBreakFast.push(new FoodMenu(this.listSnackMenu[sm].listBreakFast.length + 1, idSnackMenu, 0, 0, 0, 0, 0, 0));
+    for (let sm = 0; sm < this.listSnackMenu.length; sm++) {
+      if (idSnackMenu === this.listSnackMenu[sm].idSnackMenu) {
+        this.listSnackMenu[sm].listBreakFast.push(
+          new FoodMenu(this.listSnackMenu[sm].listBreakFast.length + 1, idSnackMenu, 0, 0, 0, 0, 0, 0)
+        );
       }
     }
   }
 
   setSnackMenu() {
-    if(this.snacks.length > this.listSnackMenu.length){
+    if (this.snacks.length > this.listSnackMenu.length) {
       this.listSnackMenu.push(this.setValueObject());
-    }else{
+    } else {
       this.snackBar.open(this.translate.instant('MENU.ERROR_SNACK_MENU'), '', {
         horizontalPosition: 'center',
         verticalPosition: 'top',
         duration: 3000
-
       });
     }
   }
 
-  private setValueObject(){
-    let sm:SnackMenu = {
+  private setValueObject() {
+    const sm: SnackMenu = {
       idSnackMenu: this.listSnackMenu.length + 1,
-      listSnackMenu: _.cloneDeep(this.snacks),
-      listBreakFast: _.cloneDeep(this.listBreakFast)
+      listSnackMenu: structuredClone(this.snacks),
+      listBreakFast: structuredClone(this.listBreakFast)
     };
 
     return sm;
@@ -175,31 +195,28 @@ export class AttendanceComponent implements OnInit {
     if (indexOfObject !== -1) {
       this.listSnackMenu.splice(indexOfObject, 1);
     }
-
   }
 
   deleteFoodMenu(food: FoodMenu) {
-    for(let sm = 0; sm < this.listSnackMenu.length; sm++){
-      if(food.idSnackMenu === this.listSnackMenu[sm].idSnackMenu){
-
+    for (let sm = 0; sm < this.listSnackMenu.length; sm++) {
+      if (food.idSnackMenu === this.listSnackMenu[sm].idSnackMenu) {
         const indexOfObject = this.listSnackMenu[sm].listBreakFast.findIndex((o: { idSnackMenu: number; }) => {
           return o.idSnackMenu === food.idSnackMenu;
         });
-    
+
         if (indexOfObject !== -1) {
           this.listSnackMenu[sm].listBreakFast.splice(indexOfObject, 1);
         }
-        
       }
     }
     this.setTotalFoodMenu();
   }
 
   totalFoodMenu(food: FoodMenu) {
-    for(let sm = 0; sm < this.listSnackMenu.length; sm++){
-      if(food.idSnackMenu === this.listSnackMenu[sm].idSnackMenu){
-        for(let f = 0; f <  this.listSnackMenu[sm].listBreakFast.length; f++){
-          if(food.id === this.listSnackMenu[sm].listBreakFast[f].id){
+    for (let sm = 0; sm < this.listSnackMenu.length; sm++) {
+      if (food.idSnackMenu === this.listSnackMenu[sm].idSnackMenu) {
+        for (let f = 0; f < this.listSnackMenu[sm].listBreakFast.length; f++) {
+          if (food.id === this.listSnackMenu[sm].listBreakFast[f].id) {
             this.listSnackMenu[sm].listBreakFast[f].idFood = food.idFood;
             this.listSnackMenu[sm].listBreakFast[f].amount = food.amount;
             this.listSnackMenu[sm].listBreakFast[f].calories = food.calories;
@@ -217,8 +234,7 @@ export class AttendanceComponent implements OnInit {
 
   setTotalFoodMenu() {
     this.resetSum();
-    debugger;
-    for(let sm = 0; sm < this.listSnackMenu.length; sm++){
+    for (let sm = 0; sm < this.listSnackMenu.length; sm++) {
       this.listSnackMenu[sm].listBreakFast.forEach(a => this.caloriesSum += a.calories);
       this.listSnackMenu[sm].listBreakFast.forEach(b => this.proteinSum += b.protein);
       this.listSnackMenu[sm].listBreakFast.forEach(c => this.lipidsSum += c.lipids);
@@ -226,13 +242,10 @@ export class AttendanceComponent implements OnInit {
     }
   }
 
-  resetSum(){
+  resetSum() {
     this.caloriesSum = 0;
     this.proteinSum = 0;
     this.carbohydrateSum = 0;
     this.lipidsSum = 0;
   }
-
-  
 }
-
